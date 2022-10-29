f11f774f2a39908db0608389170163e06d20cf04
burak <karanfilkaranfil822@gmail.com>
2022-10-29 14:46:56 +0300

    28.10.22

--- src/test/java/get_request/Get05.java
+++ src/test/java/get_request/Get05.java
@@ -35,6 +35,7 @@ public class Get05 extends RestfulBaseUrl {
         // 4. Do Assertion
         assertEquals(200,response.getStatusCode());
         assertTrue(response.asString().contains("bookingid"));
+
     }
 
 
