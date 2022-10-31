70d906443dbf10a9d82fa04406fd4308fd959e9d
burak <karanfilkaranfil822@gmail.com>
2022-10-31 20:29:11 +0300

    28.10.22

--- src/test/java/get_request/Get08.java
+++ src/test/java/get_request/Get08.java
@@ -56,15 +56,17 @@ public class Get08 extends JsonplaceholderBaseUrl {
         response.prettyPrint();
 
 //Do Assertion
+        assertEquals(200, response.statusCode());
+        assertEquals("1.1 vegur", response.header("Via"));
+        assertEquals("cloudflare", response.header("Server"));
+
         Map<String, Object> actualData = response.as(HashMap.class);//De-Serialization
         System.out.println("actualData = " + actualData);
         assertEquals(expectedData.get("userId"),actualData.get("userId"));
         assertEquals(expectedData.get("id"),actualData.get("id"));
         assertEquals(expectedData.get("title"),actualData.get("title"));
         assertEquals(expectedData.get("completed"),actualData.get("completed"));
-        assertEquals("1.1 vegur", response.header("Via"));
-        assertEquals("cloudflare", response.header("Server"));
-        assertEquals(200, response.statusCode());
+
 
     }
 }
