6f4f56894984e4bb7d5ea40277a0ce5c4d820d98
burak <karanfilkaranfil822@gmail.com>
2022-11-05 21:08:50 +0300

    03.11.22

--- src/test/java/post_request/Post05ObjectMapper_Map.java
+++ src/test/java/post_request/Post05ObjectMapper_Map.java
@@ -1,6 +1,7 @@
 package post_request;
 
 import base_url.JsonplaceholderBaseUrl;
+
 import io.restassured.http.ContentType;
 import io.restassured.response.Response;
 import org.codehaus.jackson.map.ObjectMapper;
@@ -15,7 +16,7 @@ import static io.restassured.RestAssured.given;
 import static org.junit.Assert.assertEquals;
 
 public class Post05ObjectMapper_Map extends JsonplaceholderBaseUrl {
-    /*
+     /*
          Given
            1) https://jsonplaceholder.typicode.com/todos
            2) {
@@ -23,6 +24,8 @@ public class Post05ObjectMapper_Map extends JsonplaceholderBaseUrl {
                  "title": "Tidy your room",
                  "completed": false
                }
+
+
             I send POST Request to the Url
         Then
             Status code is 201
@@ -37,30 +40,34 @@ public class Post05ObjectMapper_Map extends JsonplaceholderBaseUrl {
 
     @Test
     public void post05ObjectMapper() throws IOException {
-        //Set the URL
+        //Set the Url
         spec.pathParam("first","todos");
 
-        //Set the Expected Data
-        String jsonInString="{\n" +
-                "                                    \"userId\": 55,\n" +
-                "                                    \"title\": \"Tidy your room\",\n" +
-                "                                    \"completed\": false,\n" +
-                "                                    \"id\": 201\n" +
-                "                                    }";
-        HashMap expectedData=new ObjectMapper().readValue(jsonInString, HashMap.class);
+//Set the Expected Data
+//      String jsonInString = "{\n" +
+//              "                                    \"userId\": 55,\n" +
+//              "                                    \"title\": \"Tidy your room\",\n" +
+//              "                                    \"completed\": false,\n" +
+//              "                                    \"id\": 201\n" +
+//              "                                    }";
+        JsonPlaceHolderTestData obj = new JsonPlaceHolderTestData();
+        String jsonInString = obj.expectedDataInString(55,"Tidy your room",false);
+
+        HashMap expectedData =   new ObjectMapper().readValue(jsonInString, HashMap.class);
         System.out.println("expectedData = " + expectedData);
 
         //Send the Request and Get the Response
-        Response response =given().spec(spec).contentType(ContentType.JSON).body(expectedData).when().post("/{first}");
-    response.prettyPrint();
+        Response response = given().spec(spec).contentType(ContentType.JSON).body(expectedData).when().post("/{first}");
+        response.prettyPrint();
 
-    //Do Assertion
-       HashMap actualData= new ObjectMapper().readValue(response.asString(),HashMap.class);
+        //Do Assertion
+        HashMap actualData = new ObjectMapper().readValue(response.asString(),HashMap.class);
         System.out.println("actualData = " + actualData);
 
-        assertEquals(201,response.statusCode());
-       assertEquals(expectedData.get("completed"),actualData.get("completed"));
-       assertEquals(expectedData.get("title"),actualData.get("title"));
-       assertEquals(expectedData.get("userId"),actualData.get("userId"));
+        assertEquals(201,response.getStatusCode());
+        assertEquals(expectedData.get("completed"),actualData.get("completed"));
+        assertEquals(expectedData.get("title"),actualData.get("title"));
+        assertEquals(expectedData.get("userId"),actualData.get("userId"));
+
     }
 }
--- src/test/java/test_data/JsonPlaceHolderTestData.java
+++ src/test/java/test_data/JsonPlaceHolderTestData.java
@@ -4,17 +4,27 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class JsonPlaceHolderTestData {
-    public Map<String,Object> expectedDataMethod(Integer userId, String title, Boolean completed){
-        Map<String,Object> expectedData = new HashMap<>();
-        if(userId!=null){
-        expectedData.put("userId",userId);}
-        if(title!=null){
-        expectedData.put("title",title);}
-        if(completed!=null){
-        expectedData.put("completed",completed);}
+    public Map<String, Object> expectedDataMethod(Integer userId, String title, Boolean completed) {
+        Map<String, Object> expectedData = new HashMap<>();
+        if (userId != null) {
+            expectedData.put("userId", userId);
+        }
+        if (title != null) {
+            expectedData.put("title", title);
+        }
+        if (completed != null) {
+            expectedData.put("completed", completed);
+        }
 
-       return expectedData;
+        return expectedData;
     }
 
-
+    public String expectedDataInString(Integer userId, String title, Boolean completed) {
+        String expectedData = " {\n" +
+                "                 \"userId\": " + userId + ",\n" +
+                "                 \"title\": \"" + title + "\",\n" +
+                "                 \"completed\": " + completed + "\n" +
+                "                 }";
+        return expectedData;
+    }
 }
