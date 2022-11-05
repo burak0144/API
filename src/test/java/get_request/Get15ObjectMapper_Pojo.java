604b25498437536fec5126be28a6add0aed19191
burak <karanfilkaranfil822@gmail.com>
2022-11-05 23:17:35 +0300

    03.11.22

--- src/test/java/get_request/Get15ObjectMapper_Pojo.java
+++ src/test/java/get_request/Get15ObjectMapper_Pojo.java
@@ -1,4 +1,47 @@
 package get_request;
 
-public class Get15ObjectMapper_Pojo {
+import base_url.JsonplaceholderBaseUrl;
+import io.restassured.response.Response;
+import org.junit.Test;
+import pojos.JsonPlaceholderPojo;
+import utils.ObjectMapperUtils;
+
+import static io.restassured.RestAssured.given;
+import static org.junit.Assert.assertEquals;
+
+public class Get15ObjectMapper_Pojo extends JsonplaceholderBaseUrl {
+     /*
+        Given
+	        https://jsonplaceholder.typicode.com/todos/198
+        When
+	 		I send GET Request to the URL
+	 	Then
+	 		Status code is 200
+	 		And response body is like {
+									    "userId": 10,
+									    "id": 198,
+									    "title": "quis eius est sint explicabo",
+									    "completed": true
+									  }
+     */
+    //ObjectMapper + Pojo =En iyi yontem
+    @Test
+    public void get14Pojo() {
+        //Set the URL
+        spec.pathParams("first","todos","second",198);
+
+        //Set the Expected Data
+        JsonPlaceholderPojo expectedData=new JsonPlaceholderPojo(10,"quis eius est sint explicabo",true);
+
+        //Sent the Request and Get the Response
+        Response response = given().spec(spec).when().get("/{first}/{second}");
+
+        //Do Assertion
+        JsonPlaceholderPojo actualData=ObjectMapperUtils.convertJsonToJava(response.asString(),JsonPlaceholderPojo.class);
+
+        assertEquals(200,response.getStatusCode());
+        assertEquals(expectedData.getUserId(), actualData.getUserId());
+        assertEquals(expectedData.getTitle(), actualData.getTitle());
+        assertEquals(expectedData.getCompleted(), actualData.getCompleted());
+    }
 }
